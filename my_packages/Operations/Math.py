9fc0ba63f7d72bf617b1a6c220ad0ee0af0a2532
Unknown <ahmednoureldenmohamed@gmail.com>
2021-03-31 01:52:13 +0200

    Lecture6

---
+++ my_packages/Operations/Math.py
@@ -0,0 +1,3 @@
+def get_average(numbers):
+    average=sum(numbers)/len(numbers)
+    return  average
---
+++ my_packages/Transformers/StringTransformers.py
@@ -0,0 +1,10 @@
+def reserve_string(string_value):
+    string_value=string_value[::-1]
+    return  string_value
+
+
+def capitalize_string(string_value):
+    string_value=string_value.upper()
+    return  string_value
+
+
